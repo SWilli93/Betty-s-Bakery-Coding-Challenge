02bf387b2b242a95b7ebbfc0c8756789d851a606
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-29 12:38:27 -0500

    steps 4- 7

--- Betty's Bakery Coding Challenge.py
+++ Betty's Bakery Coding Challenge.py
@@ -4,3 +4,11 @@ cupcakes = np.array([2, .75, 2, 1, .5])
 
 recipes = np.genfromtxt('recipes.csv', delimiter=',')
 print(recipes)
+
+eggs = recipes[:,2]
+print(eggs)
+
+print(eggs == 1)
+
+cookies = recipes[2,:]
+print(cookies)
